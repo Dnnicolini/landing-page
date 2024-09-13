438993d35524e7eccd162af652795f802129683f
Dnnicolini <danienicolini@hotmail.com>
2024-09-13 11:07:36 -0400

    feat: SEO | tags, description

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,7 +22,15 @@
 <div>
 <h1 class="text-about">{{ $t('message.about') }}</h1>
 </div>
+</section>
+<section id="about" class="container d-flex justify-content-between align-items-center">
+  <div>
+    <img src="../assets/images/pc-dev.png" class="img-about"  alt="pc image">
 
+  </div>
+<div>
+<h1 class="text-about">{{ $t('message.about') }}</h1>
+</div>
 </section>
 </main>
 </template>
