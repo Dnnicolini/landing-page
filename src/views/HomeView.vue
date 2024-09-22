1e6d719df2637ae9b504ee8e62994d171b8000ce
dnnicolini <danienicolini@hotmail.com>
2024-09-22 16:30:40 -0400

    feat: Home | Background-image

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,42 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+// Defina os dados diretamente dentro do `script setup`
+const icons = ref([
+  { src: '/src/assets/images/icons/php.svg', alt: 'PHP' },
+  { src: '/src/assets/images/icons/laravel.svg', alt: 'Laravel' },
+  { src: '/src/assets/images/icons/wordpress-logo.svg', alt: 'Wordpress' },
+  { src: '/src/assets/images/icons/js-badge.svg', alt: 'JavaScript' },
+  { src: '/src/assets/images/icons/vue-js.svg', alt: 'Vue' },
+  { src: '/src/assets/images/icons/jsx-atom.svg', alt: 'React' },
+  { src: '/src/assets/images/icons/linux.svg', alt: 'Linux' },
+  { src: '/src/assets/images/icons/docker.svg', alt: 'Docker' },
+  { src: '/src/assets/images/icons/git.svg', alt: 'Git' }
+]);
+</script>
 
 <template>
   <main>
     <section
       id="initial"
-      class="container d-flex justify-content-between align-items-center"
+      class="container-fluid  justify-content-center text-center align-content-center"
     >
-      <div>
-        <h1 class="text-initial">
-          {{ $t("message.iamthe") }} <span class="text-name">Daniele Nicolini</span>
+        <h1 class="text-initial ">
+        Olá, sou Daniele Nicolini
         </h1>
-        <h4 class="text-initial">Desenvolvedora Fullstack</h4>
-        <h4 class="text-initial">EM DESENVOLVIMENTO </h4>
-      </div>
-      <div class="d-flex justify-content-center align-items-center">
-        <img
-          src="../assets/images/daniele-nicolini.png"
-          class="img-fluid dn-img me-3"
-          alt="daniele nicolini image"
-        />
-        <div class="d-grid gap-2 justify-items-center">
+        <h4 class=" text-white ">Desenvolvedora de Sistemas - PHP | Laravel | JavaScript | Vue</h4>
+        <div class="mt-3">
+
           <a
             href="https://www.linkedin.com/in/daniele-nicolini"
             target="_blank"
-            class="social-icon"
+            class="social-icon me-2"
             ><i class="bi bi-linkedin"></i
           ></a>
-          <a href="https://github.com/dnnicolini" target="_blank" class="social-icon"
+          <a href="https://github.com/dnnicolini" target="_blank" class="social-icon me-2"
             ><i class="bi bi-github"></i
           ></a>
-          <a href="https://wa.me/5569992814769" target="_blank" class="social-icon"
+          <a href="https://wa.me/5569992814769" target="_blank" class="social-icon me-2"
             ><i class="bi bi-whatsapp"></i
           ></a>
           <a
@@ -38,10 +45,8 @@
             class="social-icon"
             ><i class="bi bi-instagram"></i
           ></a>
-        </div>
       </div>
     </section>
-    <hr class="line" />
 
     <section id="about" class="container align-content-center justify-content-center">
       <h2 class="title-about">Sobre Mim</h2>
@@ -49,144 +54,23 @@
         <h4 class="text-about">{{ $t("message.about") }}</h4>
       </div>
     </section>
-    <section id="habilities" class="container">
-      <div class="row justify-content-evenly">
-        <div class="col-12 my-2 col-sm-4">
-          <div class="card text-white bg-dark">
-            <div class="card-body">
-              <h4 class="card-title">Front End</h4>
-              <div class="row my-3">
-                <h6 class="col-4">JavaScript</h6>
-                <div class="col-8 progress p-0">
-                  <div
-                    class="progress-bar "
-                    role="progressbar"
-                    style="width: 80%"
-                    aria-valuenow="90"
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                  >80%</div>
-                </div>
-              </div>
-              <div class="row my-3">
-                <h6 class="col-4">CSS/SCSS</h6>
-                <div class="col-8 progress p-0">
-                  <div
-                    class="progress-bar "
-                    role="progressbar"
-                    style="width: 80%"
-                    aria-valuenow="90"
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                  >80%</div>
-                </div>
-              </div>
-              <div class="row my-3">
-                <h6 class="col-4">Vue</h6>
-                <div class="col-8 progress p-0">
-                  <div
-                    class="progress-bar "
-                    role="progressbar"
-                    style="width: 75%"
-                    aria-valuenow="75"
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                  > 75%</div>
-                </div>
-              </div>
-              <div class="row my-3">
-                <h6 class="col-4">React</h6>
-                <div class="col-8 progress p-0">
-                  <div
-                    class="progress-bar "
-                    role="progressbar"
-                    style="width: 75%"
-                    aria-valuenow="75"
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                  >
-                  75%</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
 
-           <div class="col-12 col-sm-4">
-          <div class="card text-white bg-dark">
-            <div class="card-body">
-              <h4 class="card-title">Back End</h4>
-              <div class="row my-3">
-                <h6 class="col-4">PHP</h6>
-                <div class="col-8 progress p-0">
-                  <div
-                    class="progress-bar "
-                    role="progressbar"
-                    style="width: 90%"
-                    aria-valuenow="90"
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                  >80%</div>
-                </div>
-              </div>
-              <div class="row my-3">
-                <h6 class="col-4">C#</h6>
-                <div class="col-8 progress p-0">
-                  <div
-                    class="progress-bar "
-                    role="progressbar"
-                    style="width: 90%"
-                    aria-valuenow="90"
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                  >60%</div>
-                </div>
-              </div>
-              <div class="row my-3">
-                <h6 class="col-4">Laravel</h6>
-                <div class="col-8 progress p-0">
-                  <div
-                    class="progress-bar "
-                    role="progressbar"
-                    style="width: 80%"
-                    aria-valuenow="90"
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                  >80%</div>
-                </div>
-              </div>
-              <div class="row my-3">
-                <h6 class="col-4">Linux</h6>
-                <div class="col-8 progress p-0">
-                  <div
-                    class="progress-bar "
-                    role="progressbar"
-                    style="width: 75%"
-                    aria-valuenow="75"
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                  > 60%</div>
-                </div>
-              </div>
-              <div class="row my-3">
-                <h6 class="col-4">Mysql, Postgress</h6>
-                <div class="col-8 progress p-0">
-                  <div
-                    class="progress-bar "
-                    role="progressbar"
-                    style="width: 60%"
-                    aria-valuenow="75"
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                  >
-                  60%</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-       
-      </div>
+    <section id="habilities" class="container">
+      <h2 class="title-about">Skills</h2>
+      <transition-group name="fade-left"  appear mode="out-in" tag="div" class="row justify-content-evenly align-content-center">
+        <img v-for="(icon, index) in icons" :key="index" :src="icon.src" :alt="icon.alt" class="icon-habilities">
+      </transition-group>
     </section>
   </main>
 </template>
+
+<style>
+  /* Adicione o CSS para transição */
+  .fade-left-enter-active, .fade-left-leave-active {
+    transition: transform 2s ease, opacity 2s ease;
+  }
+  .fade-left-enter, .fade-left-leave-to {
+    transform: translateX(-30px);
+    opacity: 0;
+  }
+</style>
